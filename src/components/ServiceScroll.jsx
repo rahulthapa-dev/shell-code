3294b4c047dec06a5f7b8e0ce069818bd3dd2464
rahulthapa-dev <65037693+rahulthapa-dev@users.noreply.github.com>
2024-06-08 19:56:30 +0530

    service and blog scroll

--- src/components/ServiceScroll.jsx
+++ src/components/ServiceScroll.jsx
@@ -5,17 +5,31 @@ import Picker from 'react-mobile-picker'
 const selections = {
     serviceName: ['Offline Software Development', 'SEO', 'SMM', 'Website Development', 'Cloud', 'Devops', 'Database'],
 }
-const SelectScroll = () => {
+const SelectScroll = ({ itemHeight = 45 }) => {
     const [pickerValue, setPickerValue] = useState({
         serviceName: 'Website Development',
     })
-   
+    const pickerStyle = {
+        height: itemHeight
+    };
+    const selectedItemStyle = {
+        ...pickerStyle,
+        fontWeight: 'bold',
+        color: 'blue',
+    };
     return (
-        <Picker value={pickerValue} onChange={setPickerValue} wheelMode="natural" className='text-black font-bold bg-[#F4EF14] text-lg'>
+        <Picker value={pickerValue}
+         onChange={setPickerValue} 
+         wheelMode="natural" 
+         itemHeight={itemHeight}
+         height={itemHeight * 7}
+         itemStyle={pickerStyle}
+         selectedItemStyle={selectedItemStyle}
+         className='text-black font-bold bg-[#F4EF14] text-lg bg-scroll-custom' >
             {Object.keys(selections).map(name => (
                 <Picker.Column key={name} name={name}>
                 {selections[name].map(option => (
-                    <Picker.Item key={option} value={option} itemHeight="45">
+                    <Picker.Item key={option} value={option}>
                         {option}
                     </Picker.Item>
                 ))}
