d8ea1ca9575b8cf06c5485437b0dc5c43f8aeb21
rahulthapa-dev <65037693+rahulthapa-dev@users.noreply.github.com>
2024-06-08 12:31:04 +0530

    service carousel

---
+++ src/components/ServiceCarousel.jsx
@@ -0,0 +1,172 @@
+'use client'; 
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import '@/assets/css/app.css'; // Import your CSS file
+import Image from "next/image";
+import ServiceImage from "@/assets/img/service-img.png";
+
+// Register the ScrollTrigger plugin
+gsap.registerPlugin(ScrollTrigger);
+
+const ServiceCarousel = () => {
+  const wrapperRef = useRef(null);
+
+  useEffect(() => {
+    const sections = gsap.utils.toArray(".wrapper section");
+    let maxWidth = 0;
+
+    const getMaxWidth = () => {
+      maxWidth = 0;
+      sections.forEach((section) => {
+        maxWidth += section.offsetWidth;
+      });
+    };
+
+    getMaxWidth();
+    ScrollTrigger.addEventListener("refreshInit", getMaxWidth);
+
+    gsap.to(sections, {
+      x: () => `-${maxWidth - window.innerWidth}`,
+      ease: "none",
+      scrollTrigger: {
+        trigger: wrapperRef.current,
+        pin: true,
+        scrub: true,
+        end: () => `+=${maxWidth}`,
+        invalidateOnRefresh: true
+      }
+    });
+
+    sections.forEach((sct, i) => {
+      ScrollTrigger.create({
+        trigger: sct,
+        start: () => 'top top-=' + (sct.offsetLeft - window.innerWidth / 2) * (maxWidth / (maxWidth - window.innerWidth)),
+        end: () => '+=' + sct.offsetWidth * (maxWidth / (maxWidth - window.innerWidth)),
+        toggleClass: { targets: sct, className: "active" }
+      });
+    });
+
+    // Cleanup on unmount
+    return () => {
+      ScrollTrigger.getAll().forEach(t => t.kill());
+      gsap.killTweensOf(sections);
+    };
+  }, []);
+
+  return (
+    <div className="wrapper" ref={wrapperRef}>
+      <section>
+            <div className="pb-16">
+                <div className="mx-auto max-w-7xl px-4 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                        <div className="lg:col-span-6">
+                            <h3 className="font-clash-medium text-4xl leading-[3rem] lg:text-6xl text-white">Application <br className="lg:hidden inline-block"/> Development</h3>
+                        </div>
+                        <div className="lg:col-span-6">
+                            <div className="justify-end flex">
+                            <button className="hidden lg:inline-flex items-center gap-x-3 text-lg xl:text-2xl text-black font-clash-medium px-8 py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8 mt-5">
+                        <div className="lg:col-span-12">
+                            <div className="relative overflow-hidden service-car">
+                                <div className="service-desc lg:bg-white/90 py-6 lg:px-12 rounded-full lg:h-[525px] lg:w-[525px] flex items-center lg:justify-center lg:text-center lg:absolute lg:top-[25%] lg:right-0">
+                                    <p className="font-clash-medium text-2xl lg:text-black text-white/80">
+                                    Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                                <Image src={ServiceImage} alt="Service Image" className="rounded-lg lg:rounded-[50px]"></Image>
+                                
+                            </div>
+                            <button className="lg:hidden inline-flex items-center gap-x-3 mt-5 text-base lg:text-lg xl:text-2xl text-black font-clash-medium py-2 px-4 lg:px-8 lg:py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
+      <section>
+            <div className="pb-16">
+                <div className="mx-auto max-w-7xl px-4 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                        <div className="lg:col-span-6">
+                            <h3 className="font-clash-medium text-4xl leading-[3rem] lg:text-6xl text-white">SCO <br className="lg:hidden inline-block"/></h3>
+                        </div>
+                        <div className="lg:col-span-6">
+                            <div className="justify-end flex">
+                            <button className="hidden lg:inline-flex items-center gap-x-3 text-lg xl:text-2xl text-black font-clash-medium px-8 py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8 mt-5">
+                        <div className="lg:col-span-12">
+                            <div className="relative overflow-hidden service-car">
+                                <div className="service-desc lg:bg-white/90 py-6 lg:px-12 rounded-full lg:h-[525px] lg:w-[525px] flex items-center lg:justify-center lg:text-center lg:absolute lg:top-[25%] lg:right-0">
+                                    <p className="font-clash-medium text-2xl lg:text-black text-white/80">
+                                    Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                                <Image src={ServiceImage} alt="Service Image" className="rounded-lg lg:rounded-[50px]"></Image>
+                                
+                            </div>
+                            <button className="lg:hidden inline-flex items-center gap-x-3 mt-5 text-base lg:text-lg xl:text-2xl text-black font-clash-medium py-2 px-4 lg:px-8 lg:py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
+      <section>
+            <div className="pb-16">
+                <div className="mx-auto max-w-7xl px-4 lg:px-8">
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8">
+                        <div className="lg:col-span-6">
+                            <h3 className="font-clash-medium text-4xl leading-[3rem] lg:text-6xl text-white">DevOps <br className="lg:hidden inline-block"/></h3>
+                        </div>
+                        <div className="lg:col-span-6">
+                            <div className="justify-end flex">
+                            <button className="hidden lg:inline-flex items-center gap-x-3 text-lg xl:text-2xl text-black font-clash-medium px-8 py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="lg:grid lg:grid-cols-12 lg:gap-8 mt-5">
+                        <div className="lg:col-span-12">
+                            <div className="relative overflow-hidden service-car">
+                                <div className="service-desc lg:bg-white/90 py-6 lg:px-12 rounded-full lg:h-[525px] lg:w-[525px] flex items-center lg:justify-center lg:text-center lg:absolute lg:top-[25%] lg:right-0">
+                                    <p className="font-clash-medium text-2xl lg:text-black text-white/80">
+                                    Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.Corem ipsum dolor sit amet, consectetur adipiscing elit.
+                                    </p>
+                                </div>
+                                <Image src={ServiceImage} alt="Service Image" className="rounded-lg lg:rounded-[50px]"></Image>
+                                
+                            </div>
+                            <button className="lg:hidden inline-flex items-center gap-x-3 mt-5 text-base lg:text-lg xl:text-2xl text-black font-clash-medium py-2 px-4 lg:px-8 lg:py-3 bg-white hover:bg-[#6F0000] border border-white border-solid rounded-[79px]"><span>Know more</span> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25" />
+                                    </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+      </section>
+      {/* Add more sections as needed */}
+    </div>
+  );
+};
+
+export default ServiceCarousel;
